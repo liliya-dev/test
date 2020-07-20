dd7d9bd6a7ffd9fd478c1fb9ef68b880b8f9bbff
Liliia <chuiko1112@gmail.com>
2020-07-20 09:21:35 +0300

    sol

--- src/main.js
+++ src/main.js
@@ -1,47 +1,50 @@
 const phone = document.querySelector('#tel');
 const name = document.querySelector('#name');
-const phone_error = document.querySelector('.error-phone')
-const name_error = document.querySelector('.error-name')
-const button = document.querySelector('.form__button')
-let correct_phone = false;
-let correct_name = false;
+const phoneError = document.querySelector('.error-phone');
+const nameError = document.querySelector('.error-name');
+const button = document.querySelector('.form__button');
+let correctPhone = false;
+let correctName = false;
 
 phone.addEventListener('change', function(event) {
-    event.preventDefault();
-    const pattern = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/gmi
-    if(!pattern.test(event.target.value)) {
-        phone_error.style.display = 'block';
-        correct_phone = false;
-    } else {
-        correct_phone = true;
-    }
-})
+  event.preventDefault();
+
+  const pattern = /^((8|\+7)[ - ]?)?(\(?\d{3}\)?[ - ]?)?[\d\- ]{7,10}$/gmi;
+
+  if (!pattern.test(event.target.value)) {
+    phoneError.style.display = 'block';
+    correctPhone = false;
+  } else {
+    correctPhone = true;
+  }
+});
 
 phone.addEventListener('input', function(event) {
-    event.preventDefault();
-    phone_error.style.display = 'none';
-})
+  event.preventDefault();
+  phoneError.style.display = 'none';
+});
 
 name.addEventListener('change', function(event) {
-    console.log(111)
-    event.preventDefault();
-    if(event.target.value.trim() === ''|| /[0-9]/.test(event.target.value)) {
-        name_error.style.display = 'block';
-        correct_name = false;
-    } else {
-        correct_name = true;
-    }
-})
+  event.preventDefault();
+
+  if (event.target.value.trim() === '' || /[0-9]/.test(event.target.value)) {
+    nameError.style.display = 'block';
+    correctName = false;
+  } else {
+    correctName = true;
+  }
+});
 
 name.addEventListener('input', function(event) {
-    event.preventDefault();
-    name_error.style.display = 'none';
-})
+  event.preventDefault();
+  nameError.style.display = 'none';
+});
 
 button.addEventListener('click', function(event) {
-    event.preventDefault();
-    if(correct_name && correct_phone) {
-        phone.value = '';
-        name.value = '';
-    }
-})
+  event.preventDefault();
+
+  if (correctName && correctPhone) {
+    phone.value = '';
+    name.value = '';
+  }
+});
